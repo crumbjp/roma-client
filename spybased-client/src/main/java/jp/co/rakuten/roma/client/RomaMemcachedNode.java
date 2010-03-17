2591dfed1a042ff6c1b60cd99ff8b7a8892d0db4
crumbjp <cat.s.panch@gmail.com>
2010-03-17 16:23:01 +0900

    I/F restruction

--- spybased-client/src/main/java/jp/co/rakuten/roma/client/RomaMemcachedNode.java
+++
@@ -1,8 +0,0 @@
-package jp.co.rakuten.roma.client;
-
-import net.spy.memcached.MemcachedNode;
-
-// @@@ コネクションでマップ管理すれば、文字列では無くてインスタンス比較で対応できるかも。そしたら要らない
-public interface RomaMemcachedNode extends MemcachedNode{
-	String getName();
-}

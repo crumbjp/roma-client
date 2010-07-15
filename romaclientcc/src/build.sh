13b6046ec0ad7b370d248e1447ebe7acf4480daf
crumbjp <cat.s.panch@gmail.com>
2010-07-15 11:23:16 +0900

    -

--- romaclientcc/src/build.sh
+++ romaclientcc/src/build.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 # Test-Build & Test
-export LD_LIBRARY_PATY=`pwd`/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
 make clean
 if [ WITH_TEST == "1" ];then
     make bin/rmcc_test DEBUG=1

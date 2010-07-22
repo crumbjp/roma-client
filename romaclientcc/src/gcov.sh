1d344fc51a2391262463dae2e1db298662f515e8
junji torii <jun930@gmail.com>
2010-07-22 19:59:15 +0900

    add a flag to -DWITH_PROTOCOL_TEST

--- romaclientcc/src/gcov.sh
+++ romaclientcc/src/gcov.sh
@@ -19,7 +19,7 @@ do
 	    echo '###############'
 	    echo '# BUILD'
 	    export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-	    make clean rmcc bin/rmcc_test DEBUG=1 GCOV=1 CXXFLAGS='-DWITH_NORMAL_TEST';
+	    make clean rmcc bin/rmcc_test DEBUG=1 GCOV=1 CXXFLAGS='-DWITH_NORMAL_TEST -DWITH_PROTOCOL_TEST';
 	    ./bin/rmcc_testdc
 	    ;;
 	gen)

da1495c8d1f2ea3720b51fb3aa40558e1ee1932b
crumbjp <cat.s.panch@gmail.com>
2010-08-06 11:21:39 +0900

    PECL-phpunit

--- romaclientcc/src/PECL/build.sh
+++ romaclientcc/src/PECL/build.sh
@@ -2,13 +2,13 @@
 cd `dirname $0`
 export LD_LIBRARY_PATH=`pwd`/../lib:$LD_LIBRARY_PATH
 export TZ=Asia/Tokyo 
-#pecl-gen -f rmcc.xml
-#cd phprmcc
-#phpize
-#if [ "$WITH_CONFIGURE" == "1" ];then
-#    ./configure CPPFLAGS="-I`pwd`/../../include"
-#fi
-#make clean all test LDFLAGS=-L../../lib
+pecl-gen -f rmcc.xml
+cd phprmcc
+phpize
+if [ "$WITH_CONFIGURE" == "1" ];then
+    ./configure CPPFLAGS="-I`pwd`/../../include"
+fi
+make clean all test LDFLAGS=-L../../lib
 if [ "$WITH_TEST" == "1" ];then
     `which phpunit` --coverage-html . PhpunitEnv
     source php.env

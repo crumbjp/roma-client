91ebbdc3cba6f9a06306f08818b9f2c00227cf3e
crumbjp <cat.s.panch@gmail.com>
2010-08-06 17:02:43 +0900

    phpunit 2

--- romaclientcc/src/PECL/test/AllTest.php
+++ romaclientcc/src/PECL/test/AllTest.php
@@ -1,8 +1,9 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once 'RomaClientTest0.php';
-require_once 'RomaClientTest1.php';
+require_once 'RomaClientUnitTest0.php';
+require_once 'RomaClientUnitTest1.php';
+require_once 'RomaClientUnitTest2.php';
 class AllTest extends PHPUnit_Framework_TestSuite {
     public static function suite() {
       //return new AllTest('RomaClientTest');
@@ -10,6 +11,7 @@ class AllTest extends PHPUnit_Framework_TestSuite {
         //$suite->addTestSuite('RomaClientTest');
         $suite->addTestSuite('RomaClientUnitTest0');
         $suite->addTestSuite('RomaClientUnitTest1');
+        $suite->addTestSuite('RomaClientUnitTest2');
         return $suite;
     }
 }
